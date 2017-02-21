269e4bb03f98bf408bce2d196b512ce893489075
Jackie <jastonebr@gmail.com>
2017-02-21 01:03:39 +0000

    reorganize express directories; begin ejs demos

---
+++ ExpressServers/EJSDemo/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.render("home.ejs"); 
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+   var thing = req.params.thing;
+   res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res) {
+   var posts = [
+      {title: "Post 1", author: "Bob"},
+      {title: "Post 2", author: "Joe"},
+      {title: "Post 3", author: "Jane"} 
+    ]; 
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started.");
+});
