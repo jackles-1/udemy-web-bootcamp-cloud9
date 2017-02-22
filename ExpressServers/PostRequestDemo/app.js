2a8efac0d6c1941cb8ddc946d3041cd126035e6a
Jackie <jastonebr@gmail.com>
2017-02-22 22:58:03 +0000

    complete post request demo

--- ExpressServers/PostRequestDemo/app.js
+++ ExpressServers/PostRequestDemo/app.js
@@ -15,16 +15,13 @@ app.get("/", function(req, res){
 
 app.post("/addFriend", function(req, res){
     var newFriend = req.body.newFriend;
-    // friends.push(newFriend);
-    console.log(req.body);
-    console.log("Post Request Sent");
+    friends.push(newFriend);
+    res.redirect("/friends");
 });
 
 app.get("/friends", function(req, res){
    res.render("friends", {friends: friends}); 
 });
-
-
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server started.") ;
