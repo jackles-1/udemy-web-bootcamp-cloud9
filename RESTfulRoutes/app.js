849ec19de3e56ffac433181066801613a581d73b
Jackie <jastonebr@gmail.com>
2017-03-08 23:47:58 +0000

    complete RESTful routes blog app

--- RESTfulRoutes/app.js
+++ RESTfulRoutes/app.js
@@ -1,12 +1,17 @@
 var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+// must be after app.use(bodyParser.urlencoded...)
+app.use(expressSanitizer());
 
 var blogSchema = new mongoose.Schema({
    title: String,
@@ -22,7 +27,7 @@ app.get("/", function(req, res){
    res.redirect("/blogs"); 
 });
 
-// Index
+// INDEX
 app.get("/blogs", function(req, res){
    Blog.find({}, function(err, allBlogs){
      if(err){
@@ -41,6 +46,7 @@ app.get("/blogs/new", function(req, res){
 
 // CREATE
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, blog){
        if(err){
            console.log("Error in Create");
@@ -65,7 +71,45 @@ app.get("/blogs/:id", function(req, res){
     });
 });
 
+// EDIT
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+            console.log("Error in Edit");
+        }   
+        else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
 
+// UPDATE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err){
+           res.redirect("/");
+           console.log("Error in Update");
+       } 
+       else{
+           res.redirect("/blogs/" + req.params.id);
+       }
+    });
+});
+
+// DELETE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/");
+           console.log("Error in Delete");
+       } 
+       else{
+           res.redirect("/");
+       }
+    });
+});
 
 
 
