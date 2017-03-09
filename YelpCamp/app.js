83e92dddf998dd000af301da6e8f19f89ae9db72
Jackie <jastonebr@gmail.com>
2017-03-09 23:14:05 +0000

    start refactoring YelpCamp with modules

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,8 +1,11 @@
+// NPM Packages
 var express     = require("express"),
     app         = express(),
-    request     = require("request"),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+// Modules
+    Campground  = require("./models/campground");
+    
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 // needed to take in form data
@@ -12,32 +15,6 @@ app.use(express.static("public"));
 // tells it to assume templates are .ejs
 app.set("view engine", "ejs");
 
-// schema setup
-var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String, 
-   description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create({
-//     name: "Derpground", 
-//     image: "http://cdn-jpg2.theactivetimes.net/sites/default/files/camping.jpg",
-//     description: "This is Derpground!"
-// }, function(err, campground){
-//     if (err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("New campground: ");
-//         console.log(campground);
-//     }
-// });
-
-// http://travelchannel.sndimg.com/content/dam/images/travel/fullrights/2016/01/14/national-park-camping/camping-voyageurs-national-park-tent.jpg.rend.tccom.1280.960.jpeg
-// http://cdn-jpg2.theactivetimes.net/sites/default/files/camping.jpg
-// http://www.grindtv.com/wp-content/uploads/2015/02/shutterstock_242371765.jpg
-// http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/06/main/fall-camping-best-campgrounds-organ-pipe-cactus-national-monument-twin-peaks-1115.jpg?itok=ZIRksnnh
 
 app.get("/", function(req, res){
    res.render("landing");
